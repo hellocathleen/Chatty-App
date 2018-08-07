ca0d361fed1811794b864862fda54c4227360656
Cathleen Melendez <cathleen@melendez.com>
2018-08-07 21:29:27 +0000

    Added message components

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,40 @@ import Message from './Message.jsx';
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"},
+      messages:  [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id: "msg1"
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: "msg2"
+        }
+      ]
+    }
+  }
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      //Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      this.setState({messages: messages});
+    }, 3000)
+  }
+  
   render() {
     return (
       <div> 
-        <Message /> 
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser.name}/>
       </div>
     );
   }
