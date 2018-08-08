0ba37e67c9dd0553a7075714c059331f3a3f1fcb
Cathleen Melendez <cathleen@melendez.com>
2018-08-08 22:27:59 +0000

    Added websocket function to broadcast messages to all users

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
+import uuidv1 from 'uuid/v1';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.socket = new WebSocket("ws://localhost:3001/");
     this.state = {
       currentUser: {name: "Bob"},
       messages:  [
@@ -12,28 +14,39 @@ class App extends Component {
           username: "Bob",
           content: "Has anyone seen my marbles?",
           id: "msg1"
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: "msg2"
         }
       ]
     }
     this.updateUser = this.updateUser.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
-    //this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      //Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      this.setState({messages: messages});
-    }, 3000)
-  }
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   //Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   this.setState({messages: messages});
+    // }, 3000)
+    const ws = this.socket;
+    ws.onopen = function (event) {
+      ws.send("Here's some text that the server is urgently awaiting!"); 
+      console.log("Connected to server!")
+    }
+    const messageArray = [];
+    const oldMessages = this.state.messages;
+      ws.onmessage = function (event) {
+        console.log("event data:", event.data)
+        let newMessage = JSON.parse(event.data);
+        console.log("new message:", newMessage)
+        messageArray.push(newMessage)
+        console.log(messageArray);
+        console.log("message array", messageArray);
+        this.setState({messages: oldMessages.concat(messageArray)})
+      }.bind(this)
+  };
+    
 
   updateUser(event){
     this.setState({currentUser: {name: event.target.value}})
@@ -43,17 +56,24 @@ class App extends Component {
   handleKeyPress(event){
     if (event.keyCode === 13) {
       console.log('Enter was pressed');
-
-      const newMessage = {
+      // const newMessage = {
+      //   username: this.state.currentUser.name,
+      //   content: event.target.value,
+      //   id: new Date().toString()
+      // }
+      // const newMessages = this.state.messages.concat(newMessage)
+      // this.setState({messages: newMessages});
+      // event.target.value = "";
+      const ws = this.socket;
+      var msg = {
         username: this.state.currentUser.name,
         content: event.target.value,
-        id: new Date().toString()
+        id: uuidv1()
       }
-      const newMessages = this.state.messages.concat(newMessage)
-      this.setState({messages: newMessages});
+      this.socket.send(JSON.stringify(msg))
       event.target.value = "";
-      }
     }
+  }
 
   render() {
     return (
