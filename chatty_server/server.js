f7261e73b99f4e30fbcdd74301b7930e362e7334
Cathleen Melendez <cathleen@melendez.com>
2018-08-09 21:05:02 +0000

    Added switch for message type

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const ws = require('ws');
+const uuidv1 = require('uuid/v1');
+
 
 const PORT = 3001;
 //Create a new express server
@@ -14,13 +16,13 @@ const wss = new SocketServer({server});
 //When a client connects they are assigned a socket, represented by the ws parameter in the callback
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+  // ws.on('message', function incoming(message) {
+  //   console.log('received: %s', message);
+  // });
   ws.on('close', () => console.log('Client disconnected'));
 });
 
-let contents = '';
+
 function broadcastMessage(message) {
   for (let client of wss.clients) {
     if (client.readyState === ws.OPEN) {
@@ -29,17 +31,35 @@ function broadcastMessage(message) {
   }
 }
 
+let contents = '';
 function handleMessage(message) {
   // console.log('NEW MESSAGE!');
-  // console.log(message);
-  contents = message;
-  broadcastMessage(contents);
+
+  const parsedMsg = JSON.parse(message);
+  console.log("message:", parsedMsg.type);
+
+  switch(parsedMsg.type) {
+    case "postNotification":
+      parsedMsg.type = "incomingNotification"
+      parsedMsg['notificationId'] = uuidv1();
+      contents = JSON.stringify(parsedMsg);
+      broadcastMessage(contents);
+      break;
+    case "postMessage":
+      parsedMsg.type = "incomingMessage";
+      parsedMsg['id'] = uuidv1();
+      contents = JSON.stringify(parsedMsg);
+      broadcastMessage(contents);
+      break;
+    default:
+      throw new Error(`Unknown event type`)
+  }
 }
 
 function handleConnection(client) {
   client.on('message', handleMessage);
   // Send this new person the current state of the document!
-  client.send(contents);
+  //client.send(contents);
 }
 
 wss.on('connection', handleConnection);
