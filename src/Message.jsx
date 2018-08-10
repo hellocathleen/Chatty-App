b701c4b0926e38f788fa183d33539dd2922fc2cc
Cathleen Melendez <cathleen@melendez.com>
2018-08-10 01:50:27 +0000

    Incorporated user color into messages

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 
 
+
 class Message extends Component {
+  
   render() {
+    const fontColor = {
+      color: this.props.color
+    };
     return (
         <div className="message">
-          <span className="message-username">{this.props.username}</span>
+          <span className="message-username" style={fontColor}>{this.props.username}</span>
           <span className="message-content">{this.props.content}</span>
         </div>
     );
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -11,7 +11,7 @@ class MessageList extends Component {
           if(messageObj.type === 'incomingNotification'){
             return <Notification key={messageObj.notificationId} notification={messageObj.notification}/>
           }
-          return <Message key={messageObj.id} username={messageObj.username} content={messageObj.content} />
+          return <Message key={messageObj.id} username={messageObj.username} content={messageObj.content} color={messageObj.color} />
           })}
       </main>
     )
